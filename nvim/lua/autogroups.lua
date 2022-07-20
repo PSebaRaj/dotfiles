a6f2b083edb13b5efba3d28b2b5a8487f308a5e1
psebaraj <patrick.sebaraj@gmail.com>
2022-07-19 20:21:21 -0400

    fully ported to lua

--- nvim/lua/autogroups.lua
+++ nvim/lua/autogroups.lua
@@ -8,17 +8,28 @@ vim.api.nvim_create_autocmd({"BufWritePre"}, {
     command = "%s/\\s\\+$//e",
 })
 
+-- Eslint
 vim.api.nvim_create_autocmd({"BufWritePre"}, {
     group = PSEB_GROUP,
-    pattern = "*.tsx,*.ts,*.jsx,*.js",
+    pattern = { "*.tsx", "*.ts", "*.jsx", "*.js" } ,
     command = "EslintFixAll",
 })
 
+-- Quality of life
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "text" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
 
 -- NEED TO PORT TO LUA:
 -- Open file at place last left of at
--- if has("autocmd")
---  au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$")
---    \| exe "normal! g'\"" | endif
---endif
+vim.cmd([[
+if has("autocmd")
+	au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$")
+    \| exe "normal! g'\"" | endif
+endif
+]])
 
